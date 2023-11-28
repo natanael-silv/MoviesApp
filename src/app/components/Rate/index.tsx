abde8a4610e3a9cf3cfc3d7ab81d97ecf1868124
natanael silva <natanlimer@gmail.com>
2023-11-28 00:28:17 -0300

    fixing image src

--- src/app/components/Rate/index.tsx
+++ src/app/components/Rate/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-
+import starImage from "../../../../public/Svg/star.svg";
 interface RateProps {
   vote?: number;
 }
@@ -9,7 +9,7 @@ export default function Rate({ vote }: RateProps) {
     <>
       <span className="inline-flex items-baseline ">
         <Image
-          src="/svg/star.svg"
+          src={starImage}
           alt=""
           width={15}
           height={15}
